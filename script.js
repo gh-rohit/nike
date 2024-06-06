053b62262c53e2a0a3d5666f409b7d04913ac2f7
ojas pawar <ojaspawar0@gmail.com>
2024-06-06 16:25:38 +0530

    ten products

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function swiperSart(){
 const progressCircle = document.querySelector(".autoplay-progress svg");
 const progressContent = document.querySelector(".autoplay-progress span");
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper("#home .mySwiper", {
   loop: true,
  
   navigation: {
@@ -255,4 +255,33 @@ tl.to(".open",{
   }
 
 
+  function productswiper(){
+  const progressCircle = document.querySelector("#page5 .autoplay-progress svg");
+  const progressContent = document.querySelector("#page5 .autoplay-progress span");
+  
+  var swiper = new Swiper("#page5 .mySwiper", {
+      slidesPerView: 4,
+      loop:true,
+      grid: {
+        rows: 2,
+      },
+      spaceBetween: 20,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+      },
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+      }
+    });
+  }
 
+  productswiper()
+    
